3a1ff20d0cc262f7d24240ce40911d6e67b7a04d
jan brodzak <jan.brodzak@chyron.com>
2024-11-12 12:34:03 +0100

    tomtom search location button

--- src/components/toolbar/MapToolbar.tsx
+++ src/components/toolbar/MapToolbar.tsx
@@ -2,13 +2,39 @@ import * as React from "react";
 import { Divider, Stack, Typography } from "@mui/material";
 import AxisButton from "../AxisButton";
 import { useMap } from "../context/MapInstanceContext";
+import { services } from '@tomtom-international/web-sdk-services';
 
 export default function BasicButtonGroup() {
   const { map } = useMap();
+
+  const MOMTOM_API_KEY = "3511aLbdzJWwIOPGg5PxuE6AAARooszw";
+
+  const search = (): void => {
+    const query = document.getElementById('search') as HTMLInputElement;
+    console.log("Search ", query.value)
+    if (query.value === '') return;
+    services.fuzzySearch({
+      key: MOMTOM_API_KEY,
+      query: query.value,
+    }).then((response) => {
+      if (response && response.results && response.results.length > 0) {
+        let latitude = response.results[0]?.position?.lat as number;
+        let longitude = response.results[0]?.position?.lng as number;
+        console.log("latitude ", latitude);
+        console.log("longitude ", longitude);
+        map.setCenter({lng: longitude, lat:latitude} )
+        map.zoomTo(14, {duration: 4000});
+      }
+    });
+  }
+
   return (
     <Stack spacing={2}>
       <Typography variant="h6">Map toolbar</Typography>
-
+      <input type="text" id="search" />
+      <AxisButton onClickFunction={() => search()}>
+        Search
+      </AxisButton>
       <AxisButton onClickFunction={() => console.log("Zoom In")}>
         Zoom In
       </AxisButton>
